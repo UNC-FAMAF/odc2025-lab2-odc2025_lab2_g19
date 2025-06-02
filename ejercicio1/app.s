4651347a7d508c0aefeab1088f64b4f7e01eedee
Maximitus10 <maximo.pellegrino@mi.unc.edu.ar>
2025-06-02 13:20:38 -0300

    PUDE HACER UN CIRCULO

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -47,9 +47,16 @@ loop0:
 	mov x1, 16 // esquina superior izquierda, pos y
 	mov x2, 476 // ancho
 	mov x3, 252 // alto
-	movz w7, #0x1919, lsl 16 // color
-	movk w7, #0x0019, lsl 00
+	movz w7, #0x0019, lsl 16 // color
+	movk w7, #0x1919, lsl 00
 	bl hacer_rectangulo
+
+	mov x0, #320        // center x
+    mov x1, #240        // center y
+    mov x2, #50         // radius
+    movz w7, #0x00FF, lsl 16 // color
+	movk w7, #0xFFFF, lsl 00
+	bl hacer_circulo
 
 	// Ejemplo de uso de gpios (esto es codigo de los profes, lo dejo por las 
 	// dudas)
@@ -91,70 +98,80 @@ hacer_rectangulo: //toma x0: esquina sup. izq. x,  x1: esquina sup. izq. y,
 	add x2, x0, x2 // el largo y ancho dependen del punto de partida
 	add x3, x1, x3 // ahora tratamos x2 y x3 como los pixeles limite de dibujo 
 	mov x12, x1 //hago un contador de filas para poder ir saltando hacia abajo
-loop1_hr:
+.loop1_hr:
 	cmp x12, x3  //si llegamos a la fila limite, cortamos
-	b.eq fin_hr
+	b.eq .ret
 	// NOTA: ahora usamos x1 como nuestro puntero, no es mas la pos y
 	add x1, x0, x12 // para saltar, vamos al comienzo de la fila y sumamos x0
 	add x11, x2, x12 // seteamos el limite de dibujo en la fila:
 					 // posicion x limite mas "y" filas dibujadas
 	add x12, x12, 2560 // una vez hechos los calculos, bajamos una fila.
 					   // Esto solo afecta a la siguiente vuelta del bucle
-loop2_hr:
+.loop2_hr:
 	cmp x1, x11
-	b.eq loop1_hr // si llegamos al pixel largo limite, volvemos a sumar fila
+	b.eq .loop1_hr // si llegamos al pixel largo limite, volvemos a sumar fila
 	stur w7, [x1] //pintamos
 	add x1, x1, 4 //vamos al pixel siguiente	
-	b loop2_hr 
-fin:
-	ret
+	b .loop2_hr
 
 // PRE: x0 < 640, x1 < 480, x2 < 640 + x0, x3 < 480 + x1, w7 <= 0xFFFFFF
 // NOTA: para no buscar toda la pantalla, por eficiencia decidi que es mejor
 // buscar solo en el cuadrado en donde esta el circulo, y por eso dibujo a
 // partir de la esquina superior izquierda. Para calculrla dado un centro 
 // (x, y) y un radio r, la esquina superior se calcula como (x - r, y - r)
-// EN CONSTRUCCION
 hacer_circulo: // toma x0: esq. sup. x,  x1: esq. sup. y, x2: radio, w7: color
-	lsl x0, x0, #2
+	lsl x0, x0, #2 // *4
 	lsl x2, x2, #2
-	mov x9, 2560
-	mul x1, x1, x9 
-	add x10, x1, x20
-	add x11, x0, x10 // esq. sup. izq en el frame
-	add x12, x0, x2 // x12 va a ser el centro del circulo, le cargamos
-	                // (x - r) * 4 + r * 4 = x * 4, obteniendo la posicion
-					// en x de nuestro centro
-	mul x13, x2, x9 // calculo auxiliar: x11 = radio * 2560
-	add x13, x10, x13 // x13 = (y - r) * 2560  + r * 2560 = y * 2560 + X20,
-	                 // obtenemos la posicion del centro y en el frame
-	add x12, x12, x13 // x10 = (x + y * 640) * 4 + X20
+	mov x9, 2560 
+	mul x1, x1, x9 // *4 *640 
+	add x10, x1, x20 // esq y + fb
 	                  // obteniendo la posicion del centro (x, y) en el frame
 	lsl x13, x2, #1 // para el alto del cuadrado, guardamos el diametro (2r) en x13
-	mul x14, x13, 2560 
-	add x14, x10, x14 
-	mov x15, x10 
-loop1_hc:
+	mul x14, x13, x9 
+	add x14, x10, x14 //esy * 2560 + fb + 2(r*4) *2560
+	mov x15, x10 //x15 = esy * 640 + fb, contador arranca desde esa pos
+.loop1_hc:
 	cmp x15, x14 
-	b.eq fin
-	add x3, x0, x15
-	add x4, x2, x15
+	b.eq .ret // contador de filas igual que fila limite, cortamos
+	add x3, x0, x15 // 
+	add x4, x2, x2
+	add x4, x0, x4
+	add x4, x4, x15 // columna + 2r + contador
 	add x15, x15, 2560 
-loop2_hc:
-	cmp x3, x4
-	b.eq loop1_hc
+	mov x10, 0
+.loop2_hc:
+	add x13, x3, x10
+	cmp x13, x4
+	b.eq .loop1_hc
 	// x^2+y^2 = r. y = +-sqrt(r^2 - x^2), idem x
-	sub x5, x5, X20 //obtenemos la posicion limpia, sin framebuffer
-	sub x6, x5, x12 // obtenemos la posicion del centro en y.
-	mov x7, 640
-	udiv x6, x6, x7
+	sub x5, x3, X20 //obtenemos la posicion limpia, sin framebuffer
+	add x13, x0, x10 // pos actual x 
+	sub x6, x5, x13 // obtenemos la posicion de pos actual en y * 640
+	mov x9, 640
+	mul x12, x2, x9
+	add x12, x12, x1
+	sub x6, x6, x12
+	sdiv x6, x6, x9 // obtenemos la posicion del centro en y relativa al radio.
+	mul x11, x2, x2 // r2
+	sub x13, x10, x2 // x relativo al radio
+	mul x13, x13, x13 // x^2
+	subs x13, x11, x13 //r^2-x^2
+	b.lt .else
+	scvtf   d1, x13         // convert int → float
+	fsqrt   d0, d1         // d0 = sqrt(d1)
+	fcvtzs  x13, d0
+	cmp x6, x13 // y <= sqrt
+	b.gt .else
+	sub x13, xzr, x13
+	cmp x6, x13
+	b.lt .else
+	str w7, [x3, x10] //pintamos
+.else:
+	add x10, x10, 4 //vamos al pixel siguiente	
+	b .loop2_hc
 
-	
-
-	stur w7, [x3] //pintamos
-	add x3, x3, 4 //vamos al pixel siguiente	
-	b loop2_hc 
-
+.ret:
+	ret
 
 
 
